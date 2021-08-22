ff87fed02ee9e6f9f9939d7860c91b369477edd3
Sanket Patil <61304918+sanketpatil02@users.noreply.github.com>
2021-08-23 00:01:44 +0530

    added js file

---
+++ scripts/is-triangle.js
@@ -0,0 +1,25 @@
+const angles = document.querySelectorAll(".input-angle");
+const checkTriangle = document.querySelector("#btn");
+const outputMessage = document.querySelector("#output");
+
+function calculateSumOfAngle(angle1, angle2, angle3) {
+    const sum = angle1 + angle2 + angle3;
+    return sum;
+}
+
+function isTriangle() {
+
+    if(angles[0].value && angles[1].value && angles[0].value) {
+        const sum = calculateSumOfAngle(Number(angles[0].value), Number(angles[1].value), Number(angles[2].value) );
+
+        if(sum === 180) {
+            outputMessage.innerText = "Oh oh! Angles form triangle!";
+        } else {
+            outputMessage.innerText = "Not triangle";
+        }
+    } else {
+        outputMessage.innerText = "Please enter all three values";
+    }
+}
+
+checkTriangle.addEventListener("click", isTriangle);
